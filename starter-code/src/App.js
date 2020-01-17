001adda4d2817d066667799af20d4e6bccba5257
Mike Ta <h.michael.ta@gmail.com>
2020-01-17 11:33:08 +0100

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link } from "react-router-dom"
+import Countrydetail from "./components/Countrydetail.jsx"
+import { Switch, Route } from "react-router-dom";
+import countries from "./countries.json";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+      <div>
+        <nav class="navbar navbar-dark bg-primary mb-3">
+          <div class="container">
+            <a class="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+
+
+        <div class="container">
+          <div class="row">
+            <div class="col-5" className="items">
+              <div class="list-group">
+               
+               {countries.map((country) =>
+                <Link
+                  class="list-group-item list-group-item-action active"
+                  to={country.cca3}
+                >
+                {country.flag} {country.name.common}
+                </Link>
+               )}
+                
+
+              </div>
+            </div>
+            <Countrydetail
+            capital={countries.capital}
+            area={countries.area}
+            borders={countries.borders}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ starter-code/src/components/Countrydetail.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import countries from "../countries.json"
+import { Link } from "react-router-dom"
+
+
+export default function Countrydetail(props) {
+
+    
+  return (
+    <div class="col-7">
+      <h1>France {props.name.common}</h1>
+      <table class="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td id="capital">Capital</td>
+            <td>Paris {props.capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              551695 {props.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+
+                {countries.map((country) => 
+                    <li>
+                        <Link to={country.cca3}>{country.borders}</Link>
+                    </li>
+                )}
+                <li>
+                  <a href="/AND">Andorra</a>
+                </li>
+                <li>
+                  <a href="/BEL">Belgium</a>
+                </li>
+                <li>
+                  <a href="/DEU">Germany</a>
+                </li>
+                <li>
+                  <a href="/ITA">Italy</a>
+                </li>
+                <li>
+                  <a href="/LUX">Luxembourg</a>
+                </li>
+                <li>
+                  <a href="/MCO">Monaco</a>
+                </li>
+                <li>
+                  <a href="/ESP">Spain</a>
+                </li>
+                <li>
+                  <a href="/CHE">Switzerland</a>
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
